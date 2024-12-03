31aaf75d20a00af621af7d2fed26291923a6fbf7
Maria Rita <maria@mariarita.lustosa>
2024-12-03 09:09:44 -0300

    adição de algumas anotações, criação da tabela 'genero', e criação do atributo ID

--- entidades/Genero.java
+++ entidades/Genero.java
@@ -1,6 +1,13 @@
 import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "genero")
 public class Genero {
-    
+    @Id
+    @GeneratedValue(strategy = GeneratedType.IDENTITY)
+    private Long id;
 }
