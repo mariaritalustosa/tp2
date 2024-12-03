94c08e598159a0d21cfd28afe25e763dead27bc1
Maria Rita <maria@mariarita.lustosa>
2024-12-03 09:02:26 -0300

    criação da coluna Nome da Tabela Diretor no BD

--- entidades/Diretor.java
+++ entidades/Diretor.java
@@ -11,6 +11,6 @@ public class Diretor {
     @GeneratedValue(strategy = GeneratedType.IDENTITY)
     @Column(name ="id", nullable = false, unique = true)
     private Long id;
-    
+    @Column(name = "nome", nullable = false)
     private String nome;
 }
