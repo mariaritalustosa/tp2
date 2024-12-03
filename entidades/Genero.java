38c185bebbb7b8446c8532219c5311456b6e6926
Maria Rita <maria@mariarita.lustosa>
2024-12-03 09:12:46 -0300

    criação da coluna nome no BD edefinição do mesmo atributo na tabela Genero

--- entidades/Genero.java
+++ entidades/Genero.java
@@ -9,6 +9,12 @@ import lombok.*;
 public class Genero {
     @Id
     @GeneratedValue(strategy = GeneratedType.IDENTITY)
+    
     @Column(name = "id", nullable = false, unique = true)
     private Long id;
+
+    @Column(name = "nome", nullable = false)
+    private String nome;
+
+
 }
