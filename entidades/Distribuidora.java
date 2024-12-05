7c06d5f994f022ddc6d9b7ffb43934266994700d
Maria Rita <maria@mariarita.lustosa>
2024-12-04 21:04:21 -0300

    criação da coluna ID da tabela Distribuidora do Banco de dados, tendo como referencia o atributo id

--- entidades/Distribuidora.java
+++ entidades/Distribuidora.java
@@ -1,6 +1,14 @@
 import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "distribuidora")
 public class Distribuidora {
-    
+    @Id
+    @GeneratedValue(strategy = GeneratedType.IDENTITY)
+    @Column(name = "id", nullable = false, unique = true)
+    private Long id;
 }
