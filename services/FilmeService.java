324c190975d65cb4dc5bdc88b99b16695770a442
Maria Rita <maria@mariarita.lustosa>
2024-12-05 11:27:34 -0300

    adicionado FilmeService com injeção de dependência para FilmeRepository

---
+++ services/FilmeService.java
@@ -0,0 +1,12 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FilmeService {
+    @Autowired
+    private FilmeRepository filmeRepository;
+
+}
+
+    
+
