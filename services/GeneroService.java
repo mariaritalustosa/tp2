1380544faab0438f49c4cf25aa116d25e0672bb9
Maria Rita <maria@mariarita.lustosa>
2024-12-07 16:56:04 -0300

    adiciona injeção de dependência do GeneroRepository no GeneroService

--- services/GeneroService.java
+++ services/GeneroService.java
@@ -1,6 +1,9 @@
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GeneroService {
     
+    @Autowired
+    private GeneroRepository generoRepository;
 }
