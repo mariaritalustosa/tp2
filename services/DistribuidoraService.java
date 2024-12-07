ecce5514ce56ab4f9e57a564f4b3d516709deecf
Maria Rita <maria@mariarita.lustosa>
2024-12-07 18:36:40 -0300

    adiciona classe DistribuidoraService com injeção de dependência do DistribuidoraRepository

---
+++ services/DistribuidoraService.java
@@ -0,0 +1,8 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DistribuidoraService {
+    @Autowired
+    private DistribuidoraRepository distribuidoraRepository;
+}
