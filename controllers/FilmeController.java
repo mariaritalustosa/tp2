499f6434b6e6af7d9d89e13f65fb7410e4094db8
Maria Rita <maria@mariarita.lustosa>
2024-12-11 12:11:27 -0300

    adiciona construtor com injeção de dependência para FilmeRepository no FilmeController

--- controllers/FilmeController.java
+++ controllers/FilmeController.java
@@ -1,8 +1,13 @@
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api_filmes/filmes")
 public class FilmeController {
-    
+
+    @Autowired
+    public FilmeController(FilmeRepository repository){
+        this.repository=repository;
+    }
 }
