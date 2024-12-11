79949871e13f46858acf6517dcba9f8447c40d2d
Maria Rita <maria@mariarita.lustosa>
2024-12-11 12:18:34 -0300

    adiciona atributo FilmeRepository e método recuperarFilmes no FilmeController

--- controllers/FilmeController.java
+++ controllers/FilmeController.java
@@ -1,3 +1,5 @@
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -6,8 +8,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api_filmes/filmes")
 public class FilmeController {
 
+    private final FilmeRepository filmeRepository;
+    
     @Autowired
     public FilmeController(FilmeRepository repository){
         this.repository=repository;
     }
+
+    @GetMapping("")
+    public List<Filme> recuperarFilmes() {
+        return filmeRepository.findAll();
+    }
+    
 }
