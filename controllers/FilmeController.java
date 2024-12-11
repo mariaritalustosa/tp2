47b74a5c66f2ad6a8dc0107f5e5a609e919cf2c7
Maria Rita <maria@mariarita.lustosa>
2024-12-11 13:01:58 -0300

    adiciona endpoint para salvar filmes no FilmeController.

--- controllers/FilmeController.java
+++ controllers/FilmeController.java
@@ -1,7 +1,9 @@
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class FilmeController {
 
     private final FilmeRepository filmeRepository;
-    
+
     @Autowired
     public FilmeController(FilmeRepository repository){
         this.repository=repository;
@@ -19,5 +21,12 @@ public class FilmeController {
     public List<Filme> recuperarFilmes() {
         return filmeRepository.findAll();
     }
+
+    @PostMapping("")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Filme salvarFilme(@RequestBody Filme filmeSalvar) {
+        return filmeRepository.save(filmeSalvar);
+    }
+    
     
 }
