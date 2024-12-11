c4b113ff291f941f5ab961e6e8a2f3f99c2e6dae
Maria Rita <maria@mariarita.lustosa>
2024-12-11 13:12:02 -0300

    adiciona endpoint para atualizar filmes por ID no FilmeController

--- controllers/FilmeController.java
+++ controllers/FilmeController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("api_filmes/filmes")
@@ -28,5 +29,13 @@ public class FilmeController {
         return filmeRepository.save(filmeSalvar);
     }
     
+    @PutMapping("{id}")
+    public Filme atualizarFilme(@PathVariable("id") Long id, @RequestBody Filme paraAtualizar) {
+        return filmeRepository.findById(id)
+            .map(velho -> {
+                paraAtualizar.setId(velho.getId());
+            })
+            .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Filme não encontrado: " + id));
+    }
     
 }
