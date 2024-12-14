cd7e3ba21a5d1a2ebaafe9594383ac599c6c9571
Maria Rita <maria@mariarita.lustosa>
2024-12-14 16:09:35 -0300

    adiciona endpoint PUT para atualizar diretor existente

--- controllers/DiretorController.java
+++ controllers/DiretorController.java
@@ -1,4 +1,8 @@
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,6 +20,17 @@ public class DiretorController {
     public Diretor salvarDiretor(@RequestBody Diretor diretorSalvar) {
         return diretorRepository.save(diretorSalvar);
     }
+
+    @PutMapping("{id}")
+    public Diretor atualizarDiretor(@PathVariable("id") Long id, @RequestBody Diretor paraAtualizar) {
+        return diretorRepository.findById(id)
+            .map(diretorExistente -> {
+                paraAtualizar.setId(diretorExistente.getId());
+                return diretorRepository.save(paraAtualizar);
+            })
+            .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Diretor não encontrado: " + id));
+    }
+
     
 
     
