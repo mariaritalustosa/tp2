178f9491e21b7518d459f0d617a62d7da4fb2720
Maria Rita <maria@mariarita.lustosa>
2024-12-14 16:20:35 -0300

    cria controlador para a entidade gênero

--- controllers/DiretorController.java
+++ controllers/DiretorController.java
@@ -1,3 +1,4 @@
+import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -5,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("api_filmes/diretor")
--- controllers/FilmeController.java
+++ controllers/FilmeController.java
@@ -1,5 +1,4 @@
 import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
